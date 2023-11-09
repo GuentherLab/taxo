40f226112009367b221fa32bb68cc10b3c50b680
amsmeier <amsmeier@users.noreply.github.com>
2023-11-09 10:58:40 -0500

    Update bimodal_vs_unimodal_distribution.m

--- bimodal_vs_unimodal_distribution.m
+++ bimodal_vs_unimodal_distribution.m
@@ -1,3 +1,6 @@
+%%%% plot examples of unimodal/bimodal distributions for illustration purposes
+
+%% unimodal
 subplot(1,2,1)
 x = randn(1000000,1);
 y = randn(30000,1);
@@ -10,6 +13,8 @@ title ('Unimodal distribution')
 ylabel('Population frequency')
 xlabel('Stuttering measure')
 xlim([-4 4])
+
+%% bimodal
 subplot(1,2,2)
 x = randn(1000000,1);
 y = randn(20000,1)/3 + 4;
